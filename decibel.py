df11af02b04c5abf9b4779e2ca320b05696970a9
Hansheng Zhao <zhaohans@msu.edu>
2018-03-20 12:29:26 -0400

    init repo | init repo with the stable Decibel project, and base files for building releases.

---
+++ decibel.py
@@ -0,0 +1,147 @@
+# Author: Hansheng Zhao <copyrighthero@gmail.com> (https://www.zhs.me)
+
+
+__all__ = ('Decibel', )
+
+
+class Result(list):
+  """ Result class for Decibel results """
+
+  __slots__ = ('_rowcount', '_lastrowid')
+
+  def __init__(self, cursor):
+    """
+    Initialize list like result object
+    :param cursor: database cursor instance
+    """
+    # acquire row count and insert id
+    self._rowcount = cursor.rowcount
+    self._lastrowid = cursor.lastrowid
+    # fetch all remaining rows from cursor
+    try:
+      result = cursor.fetchall()
+    except Exception:
+      result = []
+    super(Result, self).__init__(result)
+
+  @property
+  def rowcount(self):
+    """
+    Get affect row count from cursor
+    :return: int, affected row count
+    """
+    return self._rowcount
+
+  @property
+  def lastrowid(self):
+    """
+    Get last insert row id from cursor
+    :return: int, last insert row id
+    """
+    return self._lastrowid
+
+
+class Decibel(object):
+  """ Decibel class for easy management """
+
+  __slots__ = ('_database', '_cursor', '_statements')
+
+  def __init__(self, database, statements = None):
+    """
+    Database class constructor
+    :param database: the database instance
+    """
+    # initialize local variables
+    self._database = database
+    self._cursor = database.cursor()
+    self._statements = statements \
+      if isinstance(statements, dict) else {}
+
+  def __getattr__(self, item):
+    """
+    Acquire database attributes
+    :param item: str, attribute name
+    :return: mixed, function or value
+    """
+    return self._database.__getattribute__(item)
+
+  def __call__(self, statement, value = None, many = False):
+    """
+    Call method for executing single stmt
+    :param statement: str, the statement
+    :param value: list|tuple|None, the values
+    :param many: bool, whether execute many
+    :return: list, a list of results
+    """
+    # acquire database cursor
+    cursor = self._cursor
+    # check whether values provided
+    if value is None:
+      cursor.execute(statement)
+      result = Result(cursor)
+    # check whether execute many
+    elif not many:
+      cursor.execute(statement, value)
+      result = Result(cursor)
+    else:
+      # build result list from each execution
+      result = [
+        Result(cursor) for item in value if cursor.execute(
+          statement,
+          item if isinstance(item, (tuple, list)) else (item,)
+        ) or True
+      ]
+    # commit transaction
+    self._database.commit()
+    return result
+
+  # aliases for call method
+  execute = __call__
+
+  def reg(self, stid = None, stmt = None, **kwargs):
+    """
+    Register statements in repository
+    :param stid: str|dict|None, stmt id or stmts dict
+    :param stmt: str|None, stmt corresponding to stid
+    :param kwargs: keyword args as k-v pairs
+    :return: None
+    """
+    # validate statements variable and update
+    if stid is not None:
+      if isinstance(stid, dict):
+        self._statements.update(stid)
+      elif isinstance(stmt, str):
+        self._statements.update({stid: stmt})
+    # always update kwargs
+    self._statements.update(**kwargs)
+
+  def run(self, key, value = None, many = False):
+    """
+    Execute a stored statement with value
+    :param key: the statement id
+    :param value: the optional values
+    :param many: bool, whether execute many
+    :return: list, a list of result instances
+    """
+    # check if key supplied and exist
+    if key not in self._statements: return []
+    # acquire statement from repo
+    statement = self._statements[key]
+    # invoke __call__ method
+    return self(statement, value, many)
+
+  def close(self):
+    """
+    Commit, sync and close database
+    :return: None
+    """
+    # acquire database instance
+    database = self._database
+    # commit uncommitted changes
+    database.commit()
+    # sync database to disk
+    if hasattr(database, 'sync'):
+      database.sync()
+    # close database connection
+    if hasattr(database, 'close'):
+      database.close()
---
+++ setup.py
@@ -0,0 +1,67 @@
+# Author: Hansheng Zhao <copyrighthero@gmail.com> (https://www.zhs.me)
+
+# import required setup libraries
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+# import library for metadata
+from decibel import __author__, __license__, __version__
+
+# project absolute directory
+DIRECTORY = path.abspath(path.dirname(__file__))
+
+# project readme file content
+with open(
+  path.join(DIRECTORY, 'README.rst'), encoding = 'UTF8'
+) as file_descriptor:
+  PROJECT_README = file_descriptor.read()
+
+# project required dependencies
+with open(
+  path.join(DIRECTORY, 'requirements.txt'), encoding = 'UTF8'
+) as file_descriptor:
+  REQUIREMENTS = tuple(line for line in file_descriptor if line)
+
+# project setup parameters
+setup(
+  name = 'Decibel',
+  version = __version__,
+  description = 'Python database management thin wrapper.',
+  long_description = PROJECT_README,
+  url = 'https://www.github.com/copyrighthero/Decibel',
+  download_url = 'https://www.github.com/copyrighthero/Decibel',
+  author = __author__,
+  author_email = 'copyrighthero@gmail.com',
+  license = __license__,
+  classifiers = (
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Intended Audience :: End Users/Desktop',
+    'Intended Audience :: Information Technology',
+    'Intended Audience :: Science/Research',
+    'Intended Audience :: System Administrators',
+    'License :: OSI Approved :: BSD License',
+    'License :: OSI Approved :: MIT License',
+    'Natural Language :: English',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 3',
+    'Topic :: Communications',
+    'Topic :: Database',
+    'Topic :: Internet',
+    'Topic :: Software Development',
+    'Topic :: System',
+    'Topic :: Utilities'
+  ),
+  keywords = 'Database Management Thin-wrapper Library',
+  py_modules = ("decibel", ),
+  packages = find_packages(exclude = ()),
+  install_requires = REQUIREMENTS,
+  package_data = {},
+  data_files = (),
+  entry_points = {},
+  project_urls = {
+    'Source': 'https://www.github.com/copyrighthero/Decibel'
+  }
+)
